65575d42ec7dba672c6b643eef1e6dd02278efb1
usriddle <129639843+usriddle@users.noreply.github.com>
2026-09-14 12:48:28 +0900

    fix: div -> pre 수정
    pre를 사용하여 텍스트상의 개행이 그대로 반영되도록 수정

--- project-PBA-f/src/components/SummaryViewer.jsx
+++ project-PBA-f/src/components/SummaryViewer.jsx
@@ -1,9 +1,9 @@
 export default function SummaryViewer({content}) {
   return (
     <div  style={{"display":"flex","justifyContent":"space-around"}}>
-      <div style={{"width":"600px"}}>
+      <pre style={{"width":"600px"}}>
         {content}
-      </div>
+      </pre>
     </div>
   );
 }
