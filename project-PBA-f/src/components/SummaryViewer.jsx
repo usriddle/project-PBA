34c313cae5b2d753f79ab2cf9c229fd555a88e86
usriddle <129639843+usriddle@users.noreply.github.com>
2026-09-14 14:20:43 +0900

    fix: preview에 해당하는 부분 재수정
    pre 사용시 width를 무시하고 글자가 밖으로 튀어나오는 부분을 div로 재교체 및 style을 변경하여 수정

--- project-PBA-f/src/components/SummaryViewer.jsx
+++ project-PBA-f/src/components/SummaryViewer.jsx
@@ -1,9 +1,12 @@
 export default function SummaryViewer({content}) {
   return (
-    <div  style={{"display":"flex","justifyContent":"space-around"}}>
-      <pre style={{"width":"600px"}}>
+    <div  style={{display:"flex",justifyContent:"space-around"}}>
+      <div style={{width:"600px",
+        whiteSpace: "pre-wrap",     // 개행과 공백을 유지하면서 자동 줄바꿈
+          wordBreak: "break-word"      // 긴 단어가 영역을 넘지 않도록 강제 줄바꿈
+      }}>
         {content}
-      </pre>
+      </div>
     </div>
   );
 }
